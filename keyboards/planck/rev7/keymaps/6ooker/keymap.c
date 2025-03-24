95791700e49c86deaf92bd2f866bd6ae23ff13c1
6ooker <katzenberger.erik@gmail.com>
2025-03-24 10:25:43 +0100

    New Keymap

--- keyboards/planck/rev7/keymaps/6ooker/keymap.c
+++ keyboards/planck/rev7/keymaps/6ooker/keymap.c
@@ -17,9 +17,7 @@
 #include QMK_KEYBOARD_H
 #include "keymap_german.h"
 
-enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _CURSOR };
-
-enum planck_keycodes { BACKLIT = SAFE_RANGE };
+enum planck_layers { _QWERTY, _LOWER, _RAISE, _ADJUST, _SETTINGS };
 
 typedef enum {
     TD_NONE,
@@ -41,7 +39,7 @@ typedef struct {
 // Tap Dance enums
 enum {
     TD_LOS, // OS/UNLOCK/LOCK
-    //TD_GES, // GRV,TILD/ESC
+    TD_CPY, // Copy, Select, Paste, Cut
 };
 
 td_state_t cur_dance(tap_dance_state_t *state);
@@ -49,6 +47,9 @@ td_state_t cur_dance(tap_dance_state_t *state);
 // For the Win tap dance. Put it here so it can be used in any keymap
 void win_finished(tap_dance_state_t *state, void *user_data);
 void win_reset(tap_dance_state_t *state, void *user_data);
+// Same for Copy-Paste
+void cpy_finished(tap_dance_state_t *state, void *user_data);
+void cpy_reset(tap_dance_state_t *state, void *user_data);
 
 const key_override_t ansi_scln_ovrd = ko_make_with_layers(MOD_MASK_SHIFT, DE_SCLN, DE_COLN, 1<<_QWERTY);
 const key_override_t ansi_quot_ovrd = ko_make_with_layers(MOD_MASK_SHIFT, DE_QUOT, DE_DQUO, 1<<_QWERTY);
@@ -66,7 +67,7 @@ const key_override_t *key_overrides[] = {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-#define CURSR TG(_CURSOR)
+#define SETNG TG(_SETTINGS)
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -79,14 +80,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | TDOS |      | Alt  |Lower |    Space    |Raise |AltGr |      |      |Cursr |
+ * | Ctrl | TDOS |      | Alt  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    DE_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    DE_SCLN, DE_QUOT,
     KC_LSFT, DE_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    DE_COMM, DE_DOT,  DE_SLSH, KC_ENT ,
-    KC_LCTL, TD(TD_LOS), _______, KC_LALT, LOWER, KC_RSFT, KC_SPC, RAISE,   KC_RALT, _______, _______,   CURSR
+    KC_LCTL, TD(TD_LOS), TD(TD_CPY), KC_LALT, LOWER, KC_RSFT, KC_SPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -104,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DE_TILD, DE_EXLM, DE_AT,   DE_HASH, DE_DLR,  DE_PERC, DE_CIRC, DE_AMPR,    DE_ASTR,    DE_LPRN, DE_RPRN, KC_DEL,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DE_UNDS,    DE_PLUS,    _______, _______, _______,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DE_MICR,    DE_LCBR,    DE_RCBR, DE_PIPE, _______,
-    BACKLIT, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -126,6 +127,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |  Ü   |      |  Ö   |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |  Ä   |  ß   |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      | Setn |
+ * `-----------------------------------------------------------------------------------'
+ */
+ [_ADJUST] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, DE_UDIA, _______, DE_ODIA, _______, _______,
+    _______, DE_ADIA, DE_SS  , _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SETNG
+),
+
+/* Settings
  *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
@@ -137,203 +156,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_planck_grid(
+[_SETTINGS] = LAYOUT_planck_grid(
     _______, QK_BOOT, DB_TOGG, UG_TOGG, UG_NEXT, UG_HUEU, UG_HUED, UG_SATU, UG_SATD, UG_SPDU, UG_SPDD, KC_DEL ,
     _______, EE_CLR,  MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
     _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),
-
-/* Cursor keys
- * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |  U"  |      |  O"  |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  A"  |  ss  |      |      |      | Left | Down |  Up  |Right |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
- [_CURSOR] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, DE_UDIA, _______, DE_ODIA, _______, _______,
-    _______, DE_ADIA, DE_SS  , _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
 
-#ifdef ENCODER_MAP_ENABLE
-/* Rotary Encoders
- */
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    /* Qwerty
-     *    v- (index) Clockwise / Counter Clockwise                        v- (index) Clockwise / Counter Clockwise
-     * ,---------------------------------------------------------------------------------------.
-     * | (0) Vol-    / Vol+    |   |   |   |   |   |   |   |   |   |   | (4) Vol-    / Vol+    |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (1) KC_MNXT / KC_MPRV |   |   |   |   |   |   |   |   |   |   | (5) KC_MNXT / KC_MPRV |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (2) KC_WBAK / KC_WFWD |   |   |   |   |   |   |   |   |   |   | (6) KC_SPC  / KC_ENT  |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (3) KC_LEFT / KC_RGHT |   |   |   |   |       |   |   |   |   | (7) KC_DOWN / KC_UP   |
-     * `---------------------------------------------------------------------------------------'
-     */
-    [_QWERTY] = {
-        // LEFT SIDE (index 0 to 3)
-        ENCODER_CCW_CW(KC_VOLU, KC_VOLD),
-        ENCODER_CCW_CW(KC_MNXT, KC_MPRV),
-        ENCODER_CCW_CW(KC_WBAK, KC_WFWD),
-        ENCODER_CCW_CW(KC_LEFT, KC_RGHT),
-        // RIGHT SIDE (index 4 to 7)
-        ENCODER_CCW_CW(KC_VOLU, KC_VOLD),
-        ENCODER_CCW_CW(KC_MNXT, KC_MPRV),
-        ENCODER_CCW_CW(KC_SPC,  KC_ENT),
-        ENCODER_CCW_CW(KC_DOWN, KC_UP)
-    },
-
-    /* Adjust (Lower + Raise)
-     *    v- (index) Clockwise / Counter Clockwise                        v- (index) Clockwise / Counter Clockwise
-     * ,---------------------------------------------------------------------------------------.
-     * | (0) _______ / _______ |   |   |   |   |   |   |   |   |   |   | (4) _______ / _______ |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (1) _______ / _______ |   |   |   |   |   |   |   |   |   |   | (5) _______ / _______ |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (2) UG_NEXT / UG_PREV |   |   |   |   |   |   |   |   |   |   | (6) SAT- / SAT+       |
-     * |-----------------------+---+---+---+---+---+---+---+---+---+---+-----------------------|
-     * | (3) UG_VALD / UG_VALU |   |   |   |   |       |   |   |   |   | (7) HUE- / HUE+       |
-     * `---------------------------------------------------------------------------------------'
-     */
-    [_ADJUST] = {
-        // LEFT SIDE (index 0 to 3)
-        ENCODER_CCW_CW(_______, _______),
-        ENCODER_CCW_CW(_______, _______),
-        ENCODER_CCW_CW(UG_NEXT, UG_PREV),
-        ENCODER_CCW_CW(UG_VALD, UG_VALU),
-        // RIGHT SIDE (index 4 to 7)
-        ENCODER_CCW_CW(_______, _______),
-        ENCODER_CCW_CW(_______, _______),
-        ENCODER_CCW_CW(UG_SATD,  UG_SATU),
-        ENCODER_CCW_CW(UG_HUEU,  UG_HUED)
-    }
-};
-#endif
-/* clang-format on */
-
-#ifdef AUDIO_ENABLE
-float plover_song[][2]    = SONG(PLOVER_SOUND);
-float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
-#endif
-
-bool play_encoder_melody(uint8_t index, bool clockwise);
-
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef ENCODER_MAP_ENABLE
-    if (IS_ENCODEREVENT(record->event) && record->event.pressed) {
-        play_encoder_melody(record->event.key.col, record->event.type == ENCODER_CCW_EVENT);
-    }
-#endif
-    switch (keycode) {
-        case BACKLIT:
-            if (record->event.pressed) {
-                register_code(KC_RSFT);
-            } else {
-                unregister_code(KC_RSFT);
-            }
-            return false;
-            break;
-    }
-    return true;
-}
-
-/* clang-format off */
-float melody[8][2][2] = {
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-    {{440.0f, 8}, {440.0f, 24}},
-};
-/* clang-format on */
-
-#define JUST_MINOR_THIRD 1.2
-#define JUST_MAJOR_THIRD 1.25
-#define JUST_PERFECT_FOURTH 1.33333333
-#define JUST_TRITONE 1.42222222
-#define JUST_PERFECT_FIFTH 1.33333333
-
-#define ET12_MINOR_SECOND 1.059463
-#define ET12_MAJOR_SECOND 1.122462
-#define ET12_MINOR_THIRD 1.189207
-#define ET12_MAJOR_THIRD 1.259921
-#define ET12_PERFECT_FOURTH 1.33484
-#define ET12_TRITONE 1.414214
-#define ET12_PERFECT_FIFTH 1.498307
-
-deferred_token tokens[8];
-
-uint32_t reset_note(uint32_t trigger_time, void *note) {
-    *(float*)note = 440.0f;
-    return 0;
-}
-
-bool play_encoder_melody(uint8_t index, bool clockwise) {
-    cancel_deferred_exec(tokens[index]);
-    if (clockwise) {
-        melody[index][1][0] = melody[index][1][0] * ET12_MINOR_SECOND;
-        melody[index][0][0] = melody[index][1][0] / ET12_PERFECT_FIFTH;
-        audio_play_melody(&melody[index], 2, false);
-    } else {
-        melody[index][1][0] = melody[index][1][0] / ET12_MINOR_SECOND;
-        melody[index][0][0] = melody[index][1][0] * ET12_TRITONE;
-        audio_play_melody(&melody[index], 2, false);
-    }
-    tokens[index] = defer_exec(1000, reset_note, &melody[index][1][0]);
-    return false;
-}
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    return play_encoder_melody(index, clockwise);
-}
-
-bool dip_switch_update_user(uint8_t index, bool active) {
-    switch (index) {
-        case 0: {
-#ifdef AUDIO_ENABLE
-            static bool play_sound = false;
-#endif
-            if (active) {
-#ifdef AUDIO_ENABLE
-                if (play_sound) {
-                    PLAY_SONG(plover_song);
-                }
-#endif
-                layer_on(_ADJUST);
-            } else {
-#ifdef AUDIO_ENABLE
-                if (play_sound) {
-                    PLAY_SONG(plover_gb_song);
-                }
-#endif
-                layer_off(_ADJUST);
-            }
-#ifdef AUDIO_ENABLE
-            play_sound = true;
-#endif
-            break;
-        }
-    }
-    return true;
-}
-
 
 
 td_state_t cur_dance(tap_dance_state_t *state) {
@@ -361,6 +195,11 @@ td_state_t cur_dance(tap_dance_state_t *state) {
 
 // Create an instance of 'td_tap_t' for the 'win' tap dance.
 static td_tap_t wintap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+// Same for Copy-paste
+static td_tap_t cpytap_state = {
     .is_press_action = true,
     .state = TD_NONE
 };
@@ -394,10 +233,36 @@ void win_reset(tap_dance_state_t *state, void *user_data) {
     wintap_state.state = TD_NONE;
 }
 
+void cpy_finished(tap_dance_state_t *state, void *user_data) {
+    cpytap_state.state = cur_dance(state);
+    switch (cpytap_state.state)
+    {
+    case TD_SINGLE_TAP: register_code16(C(KC_C)); break;
+    case TD_SINGLE_HOLD: register_code16(C(KC_A)); break;
+    case TD_DOUBLE_TAP: register_code16(C(KC_V)); break;
+    case TD_DOUBLE_HOLD: register_code16(C(KC_X)); break;
+    case TD_DOUBLE_SINGLE_TAP: tap_code16(C(KC_C)); register_code16(C(KC_C)); break;
+    default: break;
+    }
+}
+
+void cpy_reset(tap_dance_state_t *state, void *user_data) {
+    switch (cpytap_state.state)
+    {
+    case TD_SINGLE_TAP: unregister_code16(C(KC_C)); break;
+    case TD_SINGLE_HOLD: unregister_code16(C(KC_A)); break;
+    case TD_DOUBLE_TAP: unregister_code16(C(KC_V)); break;
+    case TD_DOUBLE_HOLD: unregister_code16(C(KC_X)); break;
+    case TD_DOUBLE_SINGLE_TAP: unregister_code16(C(KC_C)); break;
+    default: break;
+    }
+    cpytap_state.state = TD_NONE;
+}
+
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for OS, twice for Ctrl+Alt+Del, double-tap and hold for Win+L
     [TD_LOS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, win_finished, win_reset),
-    // Tap once for Grave/Tilde, double-tap for ESC
-    //[TD_GES] = ACTION_TAP_DANCE_DOUBLE(DE_GRV, KC_ESC),
+    // Tap = Copy, Hold = Select All, DTap = Paste, DTap hold = Cut
+    [TD_CPY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, cpy_finished, cpy_reset),
 };
